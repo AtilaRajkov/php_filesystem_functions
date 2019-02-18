b91b4d22c937f2ba25bbe5b850a1af62627de80d
AtilaRajkov <atilar777@gmail.com>
2019-02-18 01:33:40 +0100

    wth links

---
+++ tutorial_2/index.php
@@ -0,0 +1,101 @@
+<?php
+
+$path = '/dir1/myfile.php';
+$file = 'file1.txt';
+
+/*
+// Return filename
+echo basename($path); // myfile.php
+
+// Return fielname without ext
+echo basename($path, '.php'); // myfile
+
+// Return the directory name from path
+echo dirname($path);
+
+// Check if file exisits
+echo file_exists($file); 
+
+// Get abs path
+echo realpath($file);
+
+// Checks to see if file
+echo is_file($file);
+
+// Checks if writable
+echo is_writable($file);
+
+// Check if radeble
+echo is_readable($file);
+
+// Get file size in bytes
+echo filesize($file); // 11
+
+// Create a directory
+mkdir('testing');
+
+// Delete the directory as long there's no file in it
+// Remove dir if empty
+rmdir('testing');
+
+// Copy file
+echo copy('file1.txt', 'file2.txt');
+
+// Rename file
+rename('file2.txt', 'myfile.txt');
+
+// Delete file
+unlink('myfile.txt');
+
+// Write from file to string
+echo file_get_contents($file);
+*/
+
+// Write string to file
+
+//echo file_put_contents($file, )
+
+//echo intval("0123", 8);
+
+//$tmp = "Dragon%%Ball%%z%";
+//$arr = explode("%",trim($tmp));
+//print_r($arr);
+//echo count($arr);
+
+//function abc()
+//{
+//return __FUNCTION__;
+//}
+//function xyz()
+//{
+//return abc();
+//}
+//echo xyz();
+
+//echo TRUE;
+
+//echo '1' .  print(2) + 3 ; # 511
+
+//echo print(2) . '1'  + 3  ; # 241
+
+//echo print(1);
+
+//echo 30 * 5 . 7; # 1507
+
+//echo 2 . (3 * (print 3)); # 323
+
+//$a = 1; 
+//echo $a++;  # 1
+//$a *= 2;     
+//echo ++$a;  # 5
+
+//ucwords($file)
+
+?>
+<a href="https://granditblog.wordpress.com/2017/04/14/php-test/">1</a>
+<a href="http://upworktestru.com/php-test-2016_upwork_answers_questions/">2</a>
+<a href="http://upworkphpanswer.blogspot.com/">3</a>
+<a href="http://upworktestanswer100.blogspot.com/2017/01/upwork-php-test-answer-2017-upwork-test.html">4</a>
+<a href="http://www.w3hacking.com/php-test-2018-top-10-upwork-php-exam/">5</a>
+<a href="http://www.jomunait.com/question/upwork-php-test-question-and-answer/">6</a>
+<a href=""></a>
